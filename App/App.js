7a0df2e42c95e4736f39155c44114a20bfc6290e
Jewell <jhuffman815@gmail.com>
2020-02-18 22:55:48 -0500

    Improved functionality

--- App/App.js
+++ App/App.js
@@ -22,6 +22,8 @@ const styles = StyleSheet.create({
 export default class App extends React.Component {
   state = {
     currentValue: '0',
+    operator: null,
+    previousValue: null,
   };
 
   handleTap = (type, value) => {
@@ -33,6 +35,53 @@ export default class App extends React.Component {
         return {
           currentValue: `${state.currentValue}${value}`,
         };
+      }
+
+      if (type === 'operator') {
+        return {
+          operator: value,
+          previousValue: state.currentValue,
+          currentValue: '0',
+        };
+      }
+
+      if (type === 'equal') {
+        const { currentValue, previousValue, operator } = state;
+
+        const current = parseFloat(currentValue);
+        const previous = parseFloat(previousValue);
+        const resetState = {
+          operator: null,
+          previousValue: null,
+        };
+
+        if (operator === '/') {
+          return {
+            currentValue: previous / current,
+            ...resetState,
+          };
+        }
+
+        if (operator === '*') {
+          return {
+            currentValue: previous * current,
+            ...resetState,
+          };
+        }
+
+        if (operator === '+') {
+          return {
+            currentValue: previous + current,
+            ...resetState,
+          };
+        }
+
+        if (operator === '-') {
+          return {
+            currentValue: previous - current,
+            ...resetState,
+          };
+        }
       }
 
       return state;
@@ -55,28 +104,44 @@ export default class App extends React.Component {
               onPress={() => alert('TODO')}
             />
             <Button text="%" theme="secondary" onPress={() => alert('TODO')} />
-            <Button text="/" theme="accent" onPress={() => alert('TODO')} />
+            <Button
+              text="/"
+              theme="accent"
+              onPress={() => this.handleTap('operator', '/')}
+            />
           </Row>
 
           <Row>
             <Button text="7" onPress={() => this.handleTap('number', 7)} />
             <Button text="8" onPress={() => this.handleTap('number', 8)} />
             <Button text="9" onPress={() => this.handleTap('number', 9)} />
-            <Button text="x" theme="accent" onPress={() => alert('TODO')} />
+            <Button
+              text="x"
+              theme="accent"
+              onPress={() => this.handleTap('operator', '*')}
+            />
           </Row>
 
           <Row>
             <Button text="4" onPress={() => this.handleTap('number', 4)} />
             <Button text="5" onPress={() => this.handleTap('number', 5)} />
             <Button text="6" onPress={() => this.handleTap('number', 6)} />
-            <Button text="-" theme="accent" onPress={() => alert('TODO')} />
+            <Button
+              text="-"
+              theme="accent"
+              onPress={() => this.handleTap('operator', '-')}
+            />
           </Row>
 
           <Row>
             <Button text="1" onPress={() => this.handleTap('number', 1)} />
             <Button text="2" onPress={() => this.handleTap('number', 2)} />
             <Button text="3" onPress={() => this.handleTap('number', 3)} />
-            <Button text="+" theme="accent" onPress={() => alert('TODO')} />
+            <Button
+              text="+"
+              theme="accent"
+              onPress={() => this.handleTap('operator', '+')}
+            />
           </Row>
 
           <Row>
@@ -86,7 +151,11 @@ export default class App extends React.Component {
               onPress={() => this.handleTap('number', 0)}
             />
             <Button text="." onPress={() => this.handleTap('number', '.')} />
-            <Button text="=" theme="accent" onPress={() => alert('TODO')} />
+            <Button
+              text="="
+              theme="accent"
+              onPress={() => this.handleTap('equal')}
+            />
           </Row>
         </SafeAreaView>
       </View>
