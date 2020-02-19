ab196c399d0c4e9e297ce4047b2417535b585885
Jewell <jhuffman815@gmail.com>
2020-02-18 22:20:14 -0500

    Basic styling

--- App/components/Button.js
+++ App/components/Button.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, Dimensions } from 'react-native';
+
+const screen = Dimensions.get('window');
+const buttonWidth = screen.width / 4;
 
 const styles = StyleSheet.create({
   text: {
@@ -8,11 +11,31 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#333333',
+    flex: 1,
+    height: Math.floor(buttonWidth - 10),
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: Math.floor(buttonWidth),
+    margin: 5,
+  },
+  buttonDouble: {
+    width: screen.width / 2 - 10,
+    flex: 0,
+    alignItems: 'flex-start',
+    paddingLeft: 40,
   },
 });
 
-export default ({ onPress, text }) => (
-  <TouchableOpacity onPress={onPress} style={styles.button}>
-    <Text style={styles.text}>{text}</Text>
-  </TouchableOpacity>
-);
+export default ({ onPress, text, size }) => {
+  const buttonStyles = [styles.button];
+
+  if (size === 'double') {
+    buttonStyles.push(styles.buttonDouble);
+  }
+
+  return (
+    <TouchableOpacity onPress={onPress} style={buttonStyles}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
