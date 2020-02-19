15af7f76a852dc5c968fa32d853848566c3919a6
Jewell <jhuffman815@gmail.com>
2020-02-18 22:44:49 -0500

    Added basic functionality

--- App/App.js
+++ App/App.js
@@ -15,16 +15,38 @@ const styles = StyleSheet.create({
     fontSize: 40,
     textAlign: 'right',
     marginRight: 20,
+    marginBottom: 10,
   },
 });
 
 export default class App extends React.Component {
+  state = {
+    currentValue: '0',
+  };
+
+  handleTap = (type, value) => {
+    this.setState(state => {
+      if (type === 'number') {
+        if (state.currentValue === '0') {
+          return { currentValue: `${value}` };
+        }
+        return {
+          currentValue: `${state.currentValue}${value}`,
+        };
+      }
+
+      return state;
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <SafeAreaView>
-          <Text style={styles.value}>75</Text>
+          <Text style={styles.value}>
+            {parseFloat(this.state.currentValue).toLocaleString()}
+          </Text>
           <Row>
             <Button text="C" theme="secondary" onPress={() => alert('TODO')} />
             <Button
@@ -37,29 +59,33 @@ export default class App extends React.Component {
           </Row>
 
           <Row>
-            <Button text="7" onPress={() => alert('TODO')} />
-            <Button text="8" onPress={() => alert('TODO')} />
-            <Button text="9" onPress={() => alert('TODO')} />
+            <Button text="7" onPress={() => this.handleTap('number', 7)} />
+            <Button text="8" onPress={() => this.handleTap('number', 8)} />
+            <Button text="9" onPress={() => this.handleTap('number', 9)} />
             <Button text="x" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
-            <Button text="4" onPress={() => alert('TODO')} />
-            <Button text="5" onPress={() => alert('TODO')} />
-            <Button text="6" onPress={() => alert('TODO')} />
+            <Button text="4" onPress={() => this.handleTap('number', 4)} />
+            <Button text="5" onPress={() => this.handleTap('number', 5)} />
+            <Button text="6" onPress={() => this.handleTap('number', 6)} />
             <Button text="-" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
-            <Button text="1" onPress={() => alert('TODO')} />
-            <Button text="2" onPress={() => alert('TODO')} />
-            <Button text="3" onPress={() => alert('TODO')} />
+            <Button text="1" onPress={() => this.handleTap('number', 1)} />
+            <Button text="2" onPress={() => this.handleTap('number', 2)} />
+            <Button text="3" onPress={() => this.handleTap('number', 3)} />
             <Button text="+" theme="accent" onPress={() => alert('TODO')} />
           </Row>
 
           <Row>
-            <Button text="0" size="double" onPress={() => alert('TODO')} />
-            <Button text="." onPress={() => alert('TODO')} />
+            <Button
+              text="0"
+              size="double"
+              onPress={() => this.handleTap('number', 0)}
+            />
+            <Button text="." onPress={() => this.handleTap('number', '.')} />
             <Button text="=" theme="accent" onPress={() => alert('TODO')} />
           </Row>
         </SafeAreaView>
@@ -67,4 +93,3 @@ export default class App extends React.Component {
     );
   }
 }
-
